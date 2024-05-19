aa655cd3a118fa730a75275094100569a443b5e8
Yuliia Shatkovska <kushnir.jul@yahoo.com>
2024-05-19 19:33:37 +0300

    add docker

--- backend/helpers/connectMongo.js
+++ backend/helpers/connectMongo.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { DB_HOST } = process.env;
 
--- backend/helpers/emailSendSchedule.js
+++ backend/helpers/emailSendSchedule.js
@@ -1,10 +1,10 @@
 import cron from "node-cron";
 import { sendDailyEmail } from "../controllers/emailController.js";
 
-cron.schedule("0 9 * * *", async () => {
+cron.schedule("0 10 * * *", async () => {
   try {
     sendDailyEmail();
-    console.log("Запуск розсилки електронних листів щоденно о 9:00");
+    console.log("Запуск розсилки електронних листів щоденно о 10:00");
   } catch (error) {
     console.log(error);
   }
